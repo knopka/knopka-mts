bd63259d13b4f9dbd3902286063dc05a63cbb432
Raman Kireyeu <kireevroma@gmail.com>
2018-10-26 11:50:36 +0300

    Update tema.py

--- knopka/tema.py
+++ knopka/tema.py
@@ -2,7 +2,7 @@ import paramiko, base64
 #key = paramiko.RSAKey(data=base64.decodestring('AAA...'))
 client = paramiko.SSHClient()
 #client.get_host_keys().add('ssh.example.com', 'ssh-rsa', key)
-client.connect('172.30.32.5', username='kireev', password='4eeter')
+client.connect('172.30.32.5', username='******', password='*****')
 stdin, stdout, stderr = client.exec_command('sh run')
 for line in stdout:
     print '... ' + line.strip('\n')
